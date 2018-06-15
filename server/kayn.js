5f5da0092386a95d0dbe4797fdb48bd57d53c9a5
Ben Painter <bpaintx2@gmail.com>
2018-06-15 15:25:01 -0400

    Setup Kayn configuration

---
+++ server/kayn.js
@@ -0,0 +1,23 @@
+const { Kayn, LRUCache, REGIONS } = require('kayn');
+
+const cache = new LRUCache({ max: 1000 });
+
+let kayn = Kayn(process.env.RIOT_API_KEY)({
+	region: REGIONS.NORTH_AMERICA,
+	debugOptions: {
+		isEnabled: true,
+		showKey: true,
+	},
+	requestOptions: {
+		numberOfRetriesBeforeAbort: 3,
+		delayBeforeRetry: 3000,
+	},
+	cacheOptions: {
+		cache,
+		timeToLives: {
+			useDefault: true,
+		}
+	}
+});
+
+module.export = kayn;
