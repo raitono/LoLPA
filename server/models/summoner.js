b075b9917b430ab24a66ce709ab65ceef299f95f
Ben Painter <bpaintx2@gmail.com>
2018-06-18 17:25:32 -0400

    Beginning of track method. Change getByIdFromRiot to getByNameFromRiot.

--- server/models/summoner.js
+++ server/models/summoner.js
@@ -3,22 +3,65 @@
 let kayn = require('../kayn');
 
 module.exports = function(Summoner) {
-	Summoner.getByIdFromRiot = async function(accountId) {
+	Summoner.track = function(name, cb) {
 		return new Promise(function(resolve, reject) {
-			kayn.Summoner.by.accountID(accountId).then(function(s){
-				resolve(s);
+			Summoner.getByNameFromRiot(name)
+			.then(function(s) {
+				console.info(s);
+				Summoner.create(s)
+				.then(function () {
+					resolve(201);
+				})
+				.catch(function(reason){
+					if(reason.code === 'ER_DUP_ENTRY'){
+						let error = new Error('Tried to track a Summoner already being tracked. AccountId: ' + s.accountId);
+						error.statusCode = 409;
+						delete error.stack;
+						reject(error);
+					} else {
+						reject(reason);
+					}
+				});
 			})
 			.catch(function(reason) {
-				console.error(reason);
+				if(reason.statusCode === 404){
+					let error = new Error('Summoner name doesn\'t exist: ' + name);
+					error.statusCode = 404;
+					delete error.stack;
+					reject(error);
+				}
 			});
 		});
 	};
 	Summoner.remoteMethod(
-		'getByIdFromRiot', {
-			http: { path: '/getByIdFromRiot/:accountId', verb: 'get' },
-			accepts: { arg: 'accountId', type: 'number', required: true },
+		'track', {
+			accepts: { arg: 'name', type: 'string', required: true },
+			description: 'Add a Summoner to the database and begin tracking them'
+		}
+	);
+	
+	Summoner.getByNameFromRiot = function(name) {
+		return new Promise(function(resolve, reject) {
+			kayn.Summoner.by.name(name)
+			.then(function(s){
+				resolve(s);
+			})
+			.catch(function(reason) {
+				if(reason.statusCode === 404){
+					reject(reason);
+				} else {
+					console.error(reason);
+					reject(reason);
+				}
+			});
+		});
+	};
+	Summoner.remoteMethod(
+		'getByNameFromRiot', {
+			http: { path: '/getByNameFromRiot/:name', verb: 'get' },
+			accepts: { arg: 'name', type: 'string', required: true },
 			returns: { arg: 'Summoner', type: 'object' },
-			description: "Retrieve a Summoner from the Riot API by their Summoner id"
+			description: "Retrieve a Summoner from the Riot API by their Summoner name"
 		}
 	);
 };
