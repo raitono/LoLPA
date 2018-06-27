86ef7febc096c7c089dcc27e6e226b9190bd9f8c
Ben Painter <bpaintx2@gmail.com>
2018-06-27 17:34:39 -0400

    Outline Update function

--- worker/Summoner.js
+++ worker/Summoner.js
@@ -1,12 +1,20 @@
 const debug = require('debug')('worker:Summoner');
 
-let toUpper = function(task) {
-	debug('toUpper');
-	return task.payload.toUpperCase();
-};
-
-
+/**
+ *	Find Summoner in database
+ * 		Create if not exists
+ *	Check last update date
+ *		If null then pull most recent first before queueing history
+ *	Use last update date to pull data from Riot
+ */
+let update = function(task) {
+	debug('update');
+}
 
 module.exports.registerWorkers = function(worker) {
-	worker.registerWorker("toUpper", toUpper);
+	worker.registerWorker("toUpper", function(task) {
+		debug('toUpper');
+		return task.payload.toUpperCase();
+	});
+	worker.registerWorker("update", update);
 };
