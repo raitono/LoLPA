5eda30d6fbf86914ece89ea2249c5aeac38f38a9
Raitono <bpaintx2@gmail.com>
2018-07-11 22:40:56 -0400

    Refactor Match to use Async/Await

--- gearman/.eslintrc.js
+++ gearman/.eslintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
     "env": {
-        "browser": true,
         "node": true,
-        "mocha": true,
 		"es6":true,
     },
     "parser": "babel-eslint",
--- gearman/workers/Match.js
+++ gearman/workers/Match.js
@@ -10,79 +10,72 @@ const webServer = require('../util/web-server');
  * Pull new matches and add them to the database
  * @param {Object} summoner Summoner whose matches we are updating
  * @param {Object} options JSON options containing beginTime and endTime in UNIX milliseconds
- * @return {Promise}
  */
-let updateMatchList = (summoner) => {
+let updateMatchList = async function(summoner) {
 	try {
 		summoner = JSON.parse(summoner);
 	} catch (e) {
 		debug(e);
 	}
 
-	return getMatchList(summoner)
-		.then(() => {
-			// We have all the matches. Extract the gameIds.
-			let matchBatch = [];
-			summoner.matchList.forEach((matchList) => {
-				matchBatch.push(matchList.gameId);
-			});
+	await getMatchList(summoner);
 
-			// Send get the matches from RIOT
-			return Promise.all(matchBatch.map(Kayn.Match.get));
-		})
-		.then((matches) => {
-			let matchInsertBatch = [];
+	// We have all the matches. Extract the gameIds.
+	let matchBatch = [];
+	summoner.matchList.forEach((matchList) => {
+		matchBatch.push(matchList.gameId);
+	});
 
-			// Batch up the insert options
-			matches.forEach((match) => {
-				matchInsertBatch.push({
-					method: 'PUT',
-					uri: webServer.URLs.Matches.put(),
-					body: {
-						gameId: match.gameId,
-						seasonId: match.seasonId,
-						queueId: match.queueId,
-						mapId: match.mapId,
-						platformId: match.platformId,
-						gameVersion: match.gameVersion,
-						gameMode: match.gameMode,
-						gameType: match.gameType,
-						gameDuration: match.gameDuration,
-						gameCreation: match.gameCreation,
-					},
-					json: true,
-				});
-			});
+	// Send get the matches from RIOT
+	let matches = await Promise.all(matchBatch.map(Kayn.Match.get));
+	let matchInsertBatch = [];
 
-			// Insert the Matches
-			return Promise.all([matches, matchInsertBatch.map(request)]);
-		}).then((matchInsertResult) => {
-			// Insert match lists
-			let matchListBatch = [];
-			summoner.matchList.forEach((m) => {
-				matchListBatch.push({
-					method: 'PUT',
-					uri: webServer.URLs.MatchList.put(),
-					body: {
-						summonerId: summoner.id,
-						gameId: m.gameId,
-						championId: m.champion,
-						lane: m.lane,
-						role: m.role,
-						timestamp: m.timestamp,
-					},
-					json: true,
-				});
-			});
-
-			return Promise.all(matchListBatch.map(request));
-		}).catch((reason) => {
-			debug('General problem');
-			debug(reason);
+	// Batch up the insert options
+	matches.forEach((match) => {
+		matchInsertBatch.push({
+			method: 'PUT',
+			uri: webServer.URLs.Matches.put(),
+			body: {
+				gameId: match.gameId,
+				seasonId: match.seasonId,
+				queueId: match.queueId,
+				mapId: match.mapId,
+				platformId: match.platformId,
+				gameVersion: match.gameVersion,
+				gameMode: match.gameMode,
+				gameType: match.gameType,
+				gameDuration: match.gameDuration,
+				gameCreation: match.gameCreation,
+			},
+			json: true,
 		});
+	});
+
+	// Insert the Matches
+	await Promise.all(matchInsertBatch.map(request));
+
+	// Insert match lists
+	let matchListBatch = [];
+	summoner.matchList.forEach((m) => {
+		matchListBatch.push({
+			method: 'PUT',
+			uri: webServer.URLs.MatchList.put(),
+			body: {
+				summonerId: summoner.id,
+				gameId: m.gameId,
+				championId: m.champion,
+				lane: m.lane,
+				role: m.role,
+				timestamp: m.timestamp,
+			},
+			json: true,
+		});
+	});
+
+	await Promise.all(matchListBatch.map(request));
 };
 
-let getMatchList = (summoner, options) => {
+let getMatchList = async (summoner, options) => {
 	if (!util.isObject(summoner)) {
 		try {
 			summoner = JSON.parse(summoner);
@@ -99,97 +92,73 @@ let getMatchList = (summoner, options) => {
 	}
 
 	// Determine the dates to pull
-	return getMatchDates(options.beginTime || summoner.lastUpdated,
-		options.endTime || summoner.revisionDate)
-		.then((matchOptions) => {
-			// Send request to RIOT API
-			options = matchOptions;
-			// debug(options);
-			return Kayn.Matchlist.by.accountID(summoner.accountId).query(options);
-		}).then((riotMatchList) => {
-			// Package the options we sent and the list returned
-			return ({
-				options,
-				riotMatchList,
-			});
-		}).catch((reason) => {
-			if (reason.statusCode == 404) {
-				// This just means they don't have any matches during the time period
-				// so return the options and a null list
-				return ({
-					options,
-					riotMatchList: null,
-				});
-			} else {
-				debug('Problem calling Kayn');
-				debug(reason);
-			}
-		}).then((result) => {
-			// Determine if we can stop the recursive loop
-			if (result.options.beginTime < new Date(summoner.revisionDate).getTime()) {
-				// We are not done yet
-				if (util.isNullOrUndefined(summoner.matchList)) {
-					summoner.matchList = [];
-				}
+	options = await getMatchDates(options.beginTime || summoner.lastUpdated,
+		options.endTime || summoner.revisionDate);
 
-				if (!util.isNullOrUndefined(result.riotMatchList)) {
-					Array.prototype.push.apply(summoner.matchList, result.riotMatchList.matches);
-				}
+	let riotMatchList = null;
+	try {
+		// Send request to RIOT API
+		riotMatchList = await Kayn.Matchlist.by.accountID(summoner.accountId).query(options);
+	} catch (err) {
+		if (err.statusCode == 404) {
+			// This just means they don't have any matches during the time period
+			// so return the options and a null list
+			riotMatchList = null;
+		} else {
+			debug('Problem calling Kayn');
+			debug(err);
+		}
+	}
 
-				// Call the recursive function, but with update options and the match list attached to the summoner
-				return getMatchList(summoner, {
-					beginTime: result.options.endTime,
-					endTime: result.options.endTime + 604800000, // Add a week, the max allowed time by RIOT API
-				});
-			} else {
-				// We're done. Finish the loop.
-				return;
-			}
+	// Determine if we can stop the recursive loop
+	if (options.beginTime < new Date(summoner.revisionDate).getTime()) {
+		// We are not done yet
+		if (util.isNullOrUndefined(summoner.matchList)) {
+			summoner.matchList = [];
+		}
+
+		if (!util.isNullOrUndefined(riotMatchList)) {
+			Array.prototype.push.apply(summoner.matchList, riotMatchList.matches);
+		}
+
+		// Call the recursive function, but with update options and the match list attached to the summoner
+		return getMatchList(summoner, {
+			beginTime: options.endTime,
+			endTime: options.endTime + 604800000, // Add a week, the max allowed time by RIOT API
 		});
+	} else {
+		// We're done. Finish the loop.
+		return;
+	}
 };
 
 /**
  * @param {long} beginTime UNIX milliseconds representing the last time the Summoner was updated
  * @param {long} endTime UNIX milliseconds representing the last time the Summoner was updated according to RIOT
- * @param {Object} options OPTIONAL - JSON Object containing beginTime and endTime. If this is present, simply returns it back.
  * @return {Object} JSON object containing beginTime and endTime to be used with a match method
  */
-let getMatchDates = (beginTime, endTime) => {
-	return new Promise((resolve, reject) => {
-		if (util.isNullOrUndefined(beginTime)) {
-			return request(webServer.URLs.Season.get('{"endDate": null}'))
-				.then((dbSeason) => {
-					dbSeason = JSON.parse(dbSeason)[0];
-					let seasonStart = new Date(dbSeason.startDate).getTime();
-					resolve({
-						beginTime: seasonStart,
-						endTime: seasonStart + 604800000,
-					});
-				}).catch((reason) => {
-					debug('Problem getting current season');
-					debug(reason);
-					reject(reason);
-				});
-		} else {
-			resolve({
-				beginTime: new Date(beginTime).getTime(),
-				endTime: new Date(endTime).getTime(),
-			});
-		}
-	});
+let getMatchDates = async (beginTime, endTime) => {
+	if (util.isNullOrUndefined(beginTime)) {
+		let dbSeason = await request(webServer.URLs.Season.get('{"endDate": null}'));
+		dbSeason = JSON.parse(dbSeason)[0];
+		let seasonStart = new Date(dbSeason.startDate).getTime();
+		return {
+			beginTime: seasonStart,
+			endTime: seasonStart + 604800000,
+		};
+	} else {
+		return {
+			beginTime: new Date(beginTime).getTime(),
+			endTime: new Date(endTime).getTime(),
+		};
+	}
 };
 
 module.exports.registerWorkers = (worker) => {
-	worker.registerWorker('updateMatchList', (task) => {
-		debug('update Match List:', JSON.parse(task.payload).name);
-		updateMatchList(task.payload)
-			.then((r) => {
-				debug('End task');
-				task.end(r);
-			}).catch((reason) => {
-				debug('Problem running updateMatchList');
-				debug(reason);
-			});
+	worker.registerWorker('updateMatchList', async (task) => {
+		debug('Update Match List:', JSON.parse(task.payload).name);
+		let result = await updateMatchList(task.payload);
+		task.end(result);
 	});
 
 	debug('Workers registered');
