588163b64d260c585d5ba326b4cbf5a9cd346b64
Raitono <bpaintx2@gmail.com>
2019-02-26 18:05:10 -0500

    Add batchRequest function

--- gearman/src/util/common.ts
+++ gearman/src/util/common.ts
@@ -2,6 +2,9 @@ import * as fs from "fs";
 import requestPromiseNative = require("request-promise-native");
 import * as util from "util";
 
+// tslint:disable-next-line:no-var-requires
+const debug: any = require("debug")("lolpa-gearman:Common");
+
 const rfa = util.promisify(fs.readFile);
 
 export function request(value: requestPromiseNative.OptionsWithUri) {
@@ -10,4 +13,25 @@ export function request(value: requestPromiseNative.OptionsWithUri) {
 
 export function readFileAsync(filepath: string) {
     return rfa(filepath);
+}
+
+export async function batchRequest(requests: requestPromiseNative.OptionsWithUri[]) {
+    const k: number = parseInt(process.env.MAX_REQUEST_BATCH_SIZE);
+    const result: Array<requestPromiseNative.RequestPromise<any>> = [];
+    let n = 0;
+    for (let n = 0; n * k < requests.length; n++) {
+        const b = n * k;
+        let e = (n + 1) * k;
+
+        if (e > requests.length) {
+            e = requests.length;
+        }
+
+        debug("B: " + b);
+        debug("E: " + e);
+
+        const t = requests.slice(b, e);
+        result.concat(await Promise.all(t.map(request)));
+    }
+    return result;
 }
