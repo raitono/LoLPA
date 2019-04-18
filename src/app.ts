879330d899a96c05ec79e41c466277167254a6de
Raitono <bpaintx2@gmail.com>
2019-04-18 16:45:47 -0400

    Add routing

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ require('dotenv').config();
 const debug: any = require('debug')('app');
 
 // Third party imports
+import * as HttpStatus from 'http-status-codes';
 import Knex = require('knex');
 import { Model } from 'objection';
+import * as Koa from 'koa';
+const app:Koa = new Koa();
 
 // My imports
 
@@ -12,10 +15,21 @@ import { Model } from 'objection';
 const knex: Knex = Knex(require('../knexfile')[process.env.NODE_ENV]);
 Model.knex(knex);
 
-async function main(): Promise<any> {
-  debug('Running!');
-}
+// Generic error handling middleware.
+app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
+  try {
+    await next();
+  } catch (error) {
+    ctx.status = error.statusCode || error.status || HttpStatus.INTERNAL_SERVER_ERROR;
+    error.status = ctx.status;
+    ctx.body = { error };
+    ctx.app.emit('error', error, ctx);
+  }
+});
 
-main()
-  .catch((err) => { console.error(err); })
-  .finally(() => { return knex.destroy() });
+app.use(async (ctx:Koa.Context) => (ctx.body = 'Hello Koa!'));
+
+// Application error logging.
+app.on('error', console.error);
+
+export default app;
