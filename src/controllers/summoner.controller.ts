8d91451be5675052a13fcc8a35b56021c1fcb9ae
Raitono <bpaintx2@gmail.com>
2019-04-18 23:49:17 -0400

    Add find by name

--- src/controllers/summoner.controller.ts
+++ src/controllers/summoner.controller.ts
@@ -2,20 +2,27 @@ import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import { Summoner } from '../models/summoner';
 
-const routerOpts: Router.IRouterOptions = {
-  prefix: '/summoners',
-};
-
+const routerOpts: Router.IRouterOptions = { prefix: '/summoners' };
 const router: Router = new Router(routerOpts);
 
-router.get('/ping', async (ctx:Koa.Context) => {
+router.get('/ping', async (ctx:Router.RouterContext) => {
   ctx.body = {
     msg: 'pong'
   };
 });
 
-router.get('/', async (ctx:Koa.Context) => {
-  ctx.body = 'GET ALL';
+router.get('/:name', async (ctx:Router.RouterContext) => {
+  const sum = await Summoner.query().alias('s')
+    .where({ name: ctx.params['name'] });
+
+  if(sum[0])
+    ctx.body = sum[0];
+  else
+    ctx.status = 404;
+});
+
+router.get('/', async (ctx:Router.RouterContext) => {
+  ctx.body = await Summoner.query();
 });
 
 export default router;
